629e29b5fcaa0ab30c6bca007d9230ad1beaa2b7
RagdollCatRoll <ziniu_guo@mymail.sutd.edu.sg>
2022-03-28 21:41:20 +0800

    Update ProfileActivity.java

--- app/src/main/java/com/example/firebaseapp/profile/ProfileActivity.java
+++ app/src/main/java/com/example/firebaseapp/profile/ProfileActivity.java
@@ -1,6 +1,5 @@
 package com.example.firebaseapp.profile;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -9,8 +8,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.firebaseapp.R;
-import com.example.firebaseapp.thread.MainActivity;
-import com.example.firebaseapp.thread.PostActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -24,7 +21,7 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         // Initialize and assign variable
-        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         // Set Home selected
         bottomNavigationView.setSelectedItemId(R.id.profile);
@@ -35,16 +32,15 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch(item.getItemId())
-                {
+                switch (item.getItemId()) {
                     case R.id.dashboard:
-                        Toast.makeText(ProfileActivity.this,"Not implemented yet. You can go and do your part there!", Toast.LENGTH_LONG).show();
+                        Toast.makeText(ProfileActivity.this, "Not implemented yet. You can go and do your part there!", Toast.LENGTH_LONG).show();
                         return true;
                     case R.id.thread:
 //                        startActivity(new Intent(getApplicationContext(),MainActivity.class)
 //                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                         finish();
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.profile:
                         return true;
@@ -58,7 +54,7 @@ public class ProfileActivity extends AppCompatActivity {
     public void onBackPressed() {
         // super.onBackPressed();  // optional depending on your needs
         finish();
-        overridePendingTransition(0,0);
+        overridePendingTransition(0, 0);
 
     }
 }
